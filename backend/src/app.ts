4b0cffbab5c3673b0b4e9bed91568fd87345e77f
Piyush Kumar <piyushkumar4075@gmail.com>
2026-06-19 11:19:14 +0530

    Add ping-pong route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,9 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import errorHandler from "./middlewares/errorHandler.middleware";
 import router from "./routes";
-import { NotFoundError } from "./errors/notFoundError";
 import morgan from "morgan";
-import { getRedisClient } from "./redis/redisClient";
 
 const app = express();
 
@@ -22,20 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
-app.use("/check/:id", async (req, res) => {
-  const { id } = req.params;
-  const redisClient = getRedisClient();
-  const key = `device_${id}`;
-
-  let data = await redisClient.get(key).catch(() => null);
-  data = data && JSON.parse(data);
-
-  console.log(data);
-  res.send({ data });
-});
-
-app.use((req, res) => {
-  throw new NotFoundError("Route not found");
+app.get("/ping", (req, res) => {
+  res.status(200).send("pong");
 });
 
 app.use(errorHandler);
